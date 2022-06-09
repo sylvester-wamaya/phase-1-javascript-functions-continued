f52496767d711b744475309af33396b8605f3b10
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-09 07:08:31 +0300

    commit tree 6d4afd9c98089b7d2bed644363944546e657ed72

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  let mondayWork = function(activity = "go to the office"){
+      return `This Monday, I will ${activity}.`;
+  }
+  function wrapAdjective(mm = "*"){
+      return function(compliment ="special"){
+          return `You are ${mm}${compliment}${mm}!`;
+      }
+  }
